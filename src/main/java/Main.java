3238c43c98d360ab95b069a4050b4a98bc3c75ab
Labaile <Labaile1@gmail.com>
2022-07-14 11:40:44 -0400

    create fileWriter object to write to a file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,23 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        // your code here
+    public static void main(String[] args) throws IOException {
+        writeToFile("simple.data", "example of writing data to file");
+    }
+
+    static void writeToFile(String fileName, String text) throws IOException {
+        FileWriter fileWriter = null;
+        try {
+            fileWriter = new FileWriter(fileName);
+            fileWriter.write("example of writing data to file");
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        } finally {
+            if (fileWriter != null)
+                fileWriter.close();
+        }
     }
 }
